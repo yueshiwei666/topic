517f51009b0ff56c0a1ff3ccc16f75e8b9609276
yueshiwei666 <1467658005@qq.com>
2020-02-09 18:02:48 +0800

    浏览器和电脑有问题

--- 贪吃蛇/贪吃蛇基础小游戏/base.js
+++ 贪吃蛇/贪吃蛇基础小游戏/base.js
@@ -1,5 +1,18 @@
 //初始化地图和蛇随机开始的位置
 window.onload = function () {
+    var btn = document.getElementById("btn");
+    btn.onclick = function(){
+        start()
+    }
+    var stopp = this.document.getElementById('stop');
+    stopp.onclick = function(){
+        stop()
+    }
+
+    var step = 30
+    var direction = 'right';
+    var timer = null;/* 保存定时器 */
+
     for (let i = 0; i < 10; i++) {
         let tr = document.createElement('tr')
         for (let j = 0; j < 10; j++) {
@@ -19,43 +32,89 @@ window.onload = function () {
 
 
     /* 食物隨機出現的位置 */
-    let food = document.createElement('div');
+    var food = document.createElement('div');
     food.style.cssText='position:absolute;width:30px;height:30px;background:green;'
     table.appendChild(food);
     /* 隨即設置topleft值 */
+
+    var food_for = true;/* 决定食物的循环 */
+    var food_top;
+    var food_left;
     do{
-        let top = 30 * Math.floor(Math.random()*10) + 'px' ;
-        let left = 30 * Math.floor(Math.random()*10) + 'px' ;
-        food.style.top = top
-        food.style.left = left
-    }while( parseInt(getComputedStyle(food,null)['top']) == 0
-                               &&
-           parseInt(getComputedStyle(food,null)['left']) == 0)
+         food_top = 30 * Math.floor(Math.random()*10) + 'px' ;
+         food_left = 30 * Math.floor(Math.random()*10) + 'px' ;
+        food.style.top = food_top
+        food.style.left = food_left
+        if(!(parseInt(getComputedStyle(food,null)['top']) == 0
+                                  &&
+           parseInt(getComputedStyle(food,null)['left']) == 0)){
+         food_for = false;
+        }
+    }while(food_for) 
     
 
-    var step_left = 0;
-    var direction_left = 30
-    var direction = 'left';
-    setInterval(()=>{
-        step_left = step_left + direction_left;
-        div.style[direction] = step_left + 'px';
-        document.onkeydown = function(event){
-            //65左  87上 68右 83下  
-            event = event || window.event
-            if(event.keyCode == 65){
-                direction_left = -30;
-                direction = 'left';
-            }else if(event.keyCode == 87){
-                direction_left = -30;
-                direction = 'top'
-            }else if(event.keyCode == 68){
-                direction_left = 30;
-                direction = 'right';
-            }else if(event.keyCode == 83){
-                direction_left = 30;
-                direction = 'top'
-            }
+    function start(){
+        timer = setInterval(game,200);
+    }
+    function stop(){
+        clearInterval(timer)
+    }
+    function game(){
+        if(direction == 'left'){
+            div.style['left'] = div.offsetLeft - step + 'px';
         }
-    },500)
+        if(direction == 'right'){
+            div.style['left'] = div.offsetLeft + step + 'px';
+        }
+        if(direction == 'top'){
+            div.style['top'] = div.offsetTop - step + 'px';
+        }
+        if(direction == 'bottom'){
+            div.style['top'] = div.offsetTop + step + 'px';
+        }
+
+        if(div.offsetLeft - 1 == food.offsetLeft
+                         && 
+           div.offsetTop ==  food.offsetTop){
+                food.style.display = 'none'
+                food_for = true
+        }
+        
+        while(food_for){
+            food_top = 30 * Math.floor(Math.random()*10) + 'px' ;
+            food_left = 30 * Math.floor(Math.random()*10) + 'px' ;
+                food.style.top = food_top
+                food.style.left = food_left
+                if(food.offsetLeft == div.offsetLeft
+                                 &&
+                   food.offsetTop == food.offsetTop){
+                food_for = false;
+                food.style.display = 'block'
+                }
+        }
     
+        
+    }
+        
+    
+    /* 更改方向的函数 */
+    document.onkeydown = function(event){
+        //65左  87上 68右 83下  
+        event = event || window.event
+        /* event.witch IE6 ==  event.keyCode */
+
+        if(event.keyCode == 65){
+            
+            direction = 'left';
+        }else if(event.keyCode == 87){
+            
+            direction = 'top'
+        }else if(event.keyCode == 68){
+           
+            direction = 'right';
+        }else if(event.keyCode == 83){
+            
+            direction = 'bottom'
+        }
+    }
 }
