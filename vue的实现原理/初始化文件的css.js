5a470f017ceca6a4989e72e70c50e63eed362068
yueshiwei666 <1467658005@qq.com>
2020-03-03 17:07:48 +0800

    qqqq

---
+++ vue的实现原理/初始化文件的css.js
@@ -0,0 +1,8 @@
+/* 
+在github一个css的初始化的文件
+搜索  normalize 正常化的标椎
+出来一个normalize.css下载就行
+
+
+
+*/
---
+++ 实验室/JavaScript技术栈/原型的理解/pro.js
@@ -0,0 +1,11 @@
+/* prototype原型 */
+var fun = function(){
+    var aaa = 'qqqq'
+}   
+var d = function(){
+
+}
+var q=null
+console.log(q)
+
+
